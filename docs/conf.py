5046b01100800db9b72ff38a200ed18a2b081e77
Philip Schömig <philip.schoemig@posteo.de>
2020-08-24 19:00:12 +0200

    Comment not used paths for templates and html static files.

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+# templates_path = ["_templates"]
 
 # The master toctree document.
 master_doc = "index"
@@ -66,7 +66,7 @@ html_theme = "sphinxdoc"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 
 # -- Extension configuration -------------------------------------------------
