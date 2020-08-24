4352e60737af4bc926dae883c98b237d7860d9eb
github-action-benchmark <github@users.noreply.github.com>
2020-08-24 10:31:41 +0000

    add Benchmark (pytest) benchmark result for b52acf485dd437b734c09acf18336b8f32e7619d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1598252322478,
+  "lastUpdate": 1598265101382,
   "repoUrl": "https://github.com/philipschoemig/BER-TLV",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 7.159647548683791e-7",
             "extra": "mean: 301.92364110935597 nsec\nrounds: 196079"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "philip.schoemig@posteo.de",
+            "name": "Philip Schömig",
+            "username": "philipschoemig"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b52acf485dd437b734c09acf18336b8f32e7619d",
+          "message": "Update tests.yml",
+          "timestamp": "2020-08-24T12:30:47+02:00",
+          "tree_id": "33aca0bebca2f60c097d9b19325f984a25faa084",
+          "url": "https://github.com/philipschoemig/BER-TLV/commit/b52acf485dd437b734c09acf18336b8f32e7619d"
+        },
+        "date": 1598265100944,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_stream.py::TestBufferedStream::test_is_eof",
+            "value": 1438005.1675077316,
+            "unit": "iter/sec",
+            "range": "stddev: 9.68545690780301e-7",
+            "extra": "mean: 695.4077930979503 nsec\nrounds: 131579"
+          },
+          {
+            "name": "tests/test_stream.py::TestBufferedStream::test_size",
+            "value": 2424495.6322252303,
+            "unit": "iter/sec",
+            "range": "stddev: 9.078277353886573e-7",
+            "extra": "mean: 412.45691957852574 nsec\nrounds: 192271"
           }
         ]
       }
